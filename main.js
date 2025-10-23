10acaf2bf96a1e25a00e94eb7368a4d46b688fa2
khabbin <hvzalala@gmail.com>
2025-10-24 00:03:14 +0500

    fixed issues and task 7 is done

--- main.js
+++ main.js
@@ -1,46 +1,49 @@
 const googleUrl = 'https://google.com'
-
-const pcard = document.querySelector('.card-container');
+//задание 2
+const productCard = document.querySelector('.card-container');
 const productCards = document.querySelectorAll('.card-container');
 const changeCardColorButton = document.querySelector('#change-card-color-button');
 changeCardColorButton.addEventListener('click', () => {
-  pcard.style.backgroundColor = 'red';
+  productCard.style.backgroundColor = 'red';
 })
-
-const ChangeAllCardsColorBtn = document.querySelector('#change-all-cards-color-button');
-ChangeAllCardsColorBtn.addEventListener('click', () => {
+//задание 3
+const changeAllCardsColorBtn = document.querySelector('#change-all-cards-color-button');
+changeAllCardsColorBtn.addEventListener('click', () => {
   productCards.forEach((card) => card.style.backgroundColor = 'blue');
 })
-
+//задание 4
 const openGooglePageBtn = document.querySelector('#open-google-btn');
 openGooglePageBtn.addEventListener('click', () => {
   window.open(googleUrl);
 })
-
+//задание 5 alert
 function openGoogle() {
-  const answer = confirm('открыть?')
+  const answer = confirm('Вы действительно хотите перейти на "https://google.com"?')
+  
   if (answer === true) {
     window.open(googleUrl)
   } else {
     return;
   }
 }
+
 const openGoogleAnswerBtn = document.querySelector('#open-google-answer')
 openGoogleAnswerBtn.addEventListener('click', openGoogle)
-
+//задание 5 консоль лог
 const outputLogBtn = document.querySelector('#output-console-log-btn')
 outputLogBtn.addEventListener('click',() => openConsoleLog('privet'))
-function openConsoleLog(massage) {
-  console.log(massage)
+function openConsoleLog(message) {
+  console.log(message)
 }
-
+//задание 7
+const task7Btn1 = document.querySelector('#task-7');
 const task7Btn = document.querySelector('#task-7');
 task7Btn.addEventListener('click', () => {
-  pcard.classList.add('blue-color')
-  pcard.classList.toggle('green-color');
+  task7Btn1.classList.add('red-color-bg')
+  task7Btn1.classList.toggle('green-color-bg');
 })
-
-const mouseFocusListener = document.querySelector('.product-heading')
-mouseFocusListener.addEventListener('mouseover', () => {
-  console.log(mouseFocusListener.textContent)
+//задание 6
+const productHeading = document.querySelector('.product-heading')
+productHeading.addEventListener('mouseover', () => {
+  console.log(productHeading.textContent)
 })

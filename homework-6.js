2f38167ac19a130bddae4dceb57d02094cea5e41
khabbin <hvzalala@gmail.com>
2025-11-20 19:29:25 +0500

    homework6

---
+++ homework-6.js
@@ -0,0 +1,109 @@
+//задание 3
+const personInfo = {
+  firstName: 'Мухаммад',
+  lastName: 'Хаббин',
+  age: 19,
+  country: 'russia',
+  city: 'Sibay',
+  Employment: 'study',
+  mail: '123@gmail.com'
+}
+//задание 4
+const carInfo = {
+  brand: 'Toyota',
+  model: 'Camry',
+  year: 2022,
+  color: 'Silver Metallic',
+  transmissionType: 'Automatic',
+  maxspeed: 200
+}
+carInfo.owner = personInfo;
+console.log(carInfo)
+
+//задание 5S
+const addCarSpeedProperty = carInfoObject => {
+  if (carInfo.maxspeed === undefined) { 
+    carInfo.maxspeed = 180
+  }
+}
+addCarSpeedProperty(carInfo)
+console.log(carInfo.maxspeed)
+
+//задание 6
+const showObjectValue = (object, key) => {
+  console.log(`${key}:`, object[key])
+}
+showObjectValue(carInfo, 'model')
+
+//задание 7
+const products = ['Увлажняющий мусс', 'Увлажняющая маска', 'Гель для умывания', 'Подарочный набор 1', 'Подарочный набор 5']
+
+//задание 8
+const literaryBooks = [
+  {
+  name: '1984',
+  autor: 'Джордж Оруэлл',
+  year: 1949,
+  coverColor: 'Серый',
+  genre: 'Антиутопия'
+  },
+  {
+  name: 'Мастер и Маргарита',
+  autor: 'Михаил Булгаков',
+  year: 1966,
+  coverColor: 'Темно-синий',
+  genre: 'Роман'
+  },
+  {
+  name: 'Гарри Поттер и философский камень',
+  autor: 'Джордж Оруэлл',
+  year: 1997,
+  coverColor: 'Красный',
+  genre: 'Фэнтези'
+  }
+]
+
+literaryBooks.push( 
+  {
+  name: 'Убить пересмешника',
+  autor: 'Харпер Ли',
+  year: 1960,
+  coverColor: 'Белый',
+  genre: 'Роман'
+  }
+)
+console.log(literaryBooks)
+
+//задание 9
+const marvelBooks = [
+  {
+  name: 'Человек-паук: Синий',
+  autor: 'Джеф Лоуб',
+  year: 2002 ,
+  coverColor: 'Синий ',
+  genre: 'Супергероика'
+  },
+  {
+  name: 'Гражданская война',
+  autor: 'Марк Миллар',
+  year: 2006,
+  coverColor: 'Красный',
+  genre: 'Супергероика'
+  },
+  {
+  name: 'Дэдпул уничтожает вселенную Марвел',
+  autor: 'Каллен Банн',
+  year: 2011,
+  coverColor: 'Красный',
+  genre: 'Супергероика'
+  }
+]
+const combinedBooks = [...literaryBooks, ...marvelBooks]
+console.log(combinedBooks)
+
+// задание 10
+const booksRarityArray = combinedBooks.map((book) => {
+  return {...book, rarity : book.year <= 2000 ? true : false}
+})
+
+console.log(booksRarityArray)

4b154a2da4a406ff7ea25079be708315390339c2
khabbin <hvzalala@gmail.com>
2025-12-10 15:07:11 +0500

    proverka

--- main.js
+++ main.js
@@ -1,47 +1,8 @@
-// //задание 2
-// const productCard = document.querySelector('.card-container');
-// const productCards = document.querySelectorAll('.card-container');
-// const changeCardColorButton = document.querySelector('#change-card-color-button');
-// changeCardColorButton.addEventListener('click', () => {
-//   productCard.style.backgroundColor = 'red';
-// })
-// //задание 3
-// const changeAllCardsColorBtn = document.querySelector('#change-all-cards-color-button');
-// changeAllCardsColorBtn.addEventListener('click', () => {
-//   productCards.forEach((card) => card.style.backgroundColor = 'blue');
-// })
-// //задание 4
-// const googleUrl = 'https://google.com'
-// const openGooglePageBtn = document.querySelector('#open-google-btn');
-// openGooglePageBtn.addEventListener('click', () => {
-//   window.open(googleUrl);
-// })
-// //задание 5 alert
-// function openGoogle() {
-//   const answer = confirm('Вы действительно хотите перейти на "https://google.com"?')
-  
-//   if (answer === true) {
-//     window.open(googleUrl)
-//   } else {
-//     return;
-//   }
-// }
-
-// const openGoogleAnswerBtn = document.querySelector('#open-google-answer')
-// openGoogleAnswerBtn.addEventListener('click', openGoogle)
-// //задание 5 консоль лог
-// const outputLogBtn = document.querySelector('#output-console-log-btn')
-// outputLogBtn.addEventListener('click',() => openConsoleLog('privet'))
-// function openConsoleLog(message) {
-//   console.log(message)
-// }
-// //задание 7
-// const colorСhangingBtn = document.querySelector('#color-change-btn');
-// colorСhangingBtn.addEventListener('click', () => {
-//   colorСhangingBtn.classList.toggle('green-color-bg');
-// })
-// //задание 6
-// const mainTitle = document.querySelector('.product-heading')
-// mainTitle.addEventListener('mouseover', () => {
-//   console.log(mainTitle.textContent)
-// })
+export * from './homework-9.js'
+export * from './homework-8.js'
+export * from './homework-7.js'
+export * from './homework-6.js'
+export * from './homework-5.js'
+export * from './comments.js'
+export * from './product-cards.js'
+import './homework-10.js'

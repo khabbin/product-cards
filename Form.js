1b81255f6c1ca442ac1625f023bb40ba81c98801
khabbin <hvzalala@gmail.com>
2025-12-12 12:06:40 +0500

    homework-10-3 fix issues

--- Form.js
+++ Form.js
@@ -2,16 +2,17 @@ export class Form {
   constructor(id) {
     this.form = document.getElementById(id)
   }
+  
   getFormData() {
     const formData = new FormData(this.form);
     const data = Object.fromEntries(formData);
     return data;
   }
+  
   isValid(){
-    if (!this.form.checkValidity()) {
-      return false
-    } else return true
+    return this.form.checkValidity()
   }
+  
   resetForm() {
     this.form.reset()
   }
--- Modal.js
+++ Modal.js
@@ -1,16 +1,17 @@
 export class Modal {
   constructor(id) {
-    this.ModalElement = document.getElementById(id)
+    this.modalElement = document.getElementById(id)
   }
+  
   openModal() {
-    this.ModalElement.style.visibility = 'visible'
+    this.modalElement.style.visibility = 'visible'
   }
+  
   closeModal() {
-   this.ModalElement.style.visibility = 'hidden' 
+   this.modalElement.style.visibility = 'hidden' 
   }
+  
   isModalOpen() {
-    if (this.ModalElement.style.visibility === 'visible') {
-      return true
-    } else return false
+    return this.modalElement.style.visibility === 'visible'
   }
-}
+} 

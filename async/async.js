67d793b41700704f96987a02d2414a974f51bb4b
khabbin <hvzalala@gmail.com>
2026-01-07 01:05:22 +0500

    issues fixed

--- async/async.js
+++ async/async.js
@@ -1,45 +1,48 @@
-const loadingTextElement = document.getElementById('loading-text')
-const errorTextElement = document.getElementById('error-text')
-const usersTemplate = document.getElementById('users-template')
-const usersList = document.querySelector('.users-list')
+const loadingTextElement = document.getElementById('loading-text');
+const errorTextElement = document.getElementById('error-text');
+const usersTemplate = document.getElementById('users-template');
+const usersList = document.querySelector('.users-list');
 
-const showLoadingText = (boolean) => loadingTextElement.style.visibility = boolean ? 'visible' : 'hidden'
-const showErrorText = (boolean) => errorTextElement.style.visibility = boolean ? 'visible' : 'hidden'
+const getLocalData = () => {
+  return JSON.parse(localStorage.getItem('usersData')) || [];
+}
+const setLoadingVisible = (isVisible) => loadingTextElement.style.visibility = isVisible ? 'visible' : 'hidden';
+const setErrorVisible = (isVisible) => errorTextElement.style.visibility = isVisible ? 'visible' : 'hidden';
 
-const deleteAllUsersBtn = document.getElementById('delete-all-users-btn')
+const deleteAllUsersBtn = document.getElementById('delete-all-users-btn');
 deleteAllUsersBtn.addEventListener('click', () => {
-  localStorage.setItem('usersData', JSON.stringify([]));
-  displayUsers()
-})
+  localStorage.removeItem('usersData');
+  displayUsers([]);
+});
 
-const showAllUsersBtn = document.getElementById('show-all-users-btn')
-showAllUsersBtn.addEventListener('click', () => {
-  loadUsers()
-  displayUsers()
-})
+const showAllUsersBtn = document.getElementById('show-all-users-btn');
+showAllUsersBtn.addEventListener('click', async () => {
+  usersList.innerHTML = '';
+  const users = await loadUsers();
+  displayUsers(users);
+});
 
-const deleteUserBtn = document.getElementById('delete-user-btn')
+const deleteUserBtn = document.getElementById('delete-user-btn');
 deleteUserBtn.addEventListener('click', () => {
-  let deleteId = Number(prompt('Удалить юзера с id:'))
-  const cards = JSON.parse(localStorage.getItem('usersData')) || [];
+  let deleteId = Number(prompt('Удалить юзера с id:'));
+  const cards = getLocalData();
+  const updatedCards = cards.filter(card => card.id !== deleteId);
   if (cards.some(card => card.id === deleteId)) {
-    const updatedCards = cards.filter(card => card.id !== deleteId);
     localStorage.setItem('usersData', JSON.stringify(updatedCards));
   } else {
-    alert('Юзер с таким id не найден')
+    alert('Юзер с таким id не найден');
     throw new Error('Такого id нет');
   }
-  displayUsers()
-})
+  displayUsers(updatedCards);
+});
 
-const clearStorageBtn = document.getElementById('clear-storage-btn')
+const clearStorageBtn = document.getElementById('clear-storage-btn');
 clearStorageBtn.addEventListener('click', () => {
     localStorage.clear();
 });
 
-const displayUsers = () => {
+const displayUsers = (users) => {
   usersList.innerHTML = '';
-  const users = JSON.parse(localStorage.getItem('usersData'));
   users.forEach((user) => {
     const userCardClone = usersTemplate.content.cloneNode(true);
     userCardClone.querySelector('#user-id').textContent = `ID: ${ user.id }`;
@@ -52,30 +55,31 @@ const displayUsers = () => {
 }
 
 async function loadUsers() {
-  showLoadingText(true);
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  const response = await fetch('./async.json');
-  if (!response.ok) {
-    showLoadingText(false);
-    showErrorText(true);
-    throw new Error('Ошибка загрузки данных');
+  setErrorVisible(false);
+  setLoadingVisible(true);
+  try {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    const response = await fetch('./async.json');
+    if (!response.ok) {
+      throw new Error('Ошибка загрузки');
+    }
+    const fetchedData = await response.json();
+    localStorage.setItem('usersData', JSON.stringify(fetchedData));
+    return fetchedData;
+  } catch (error) {
+    setErrorVisible(true);
+    return [];
+  } finally {
+    setLoadingVisible(false);
   }
-
-  showLoadingText(false);
-  const data = await response.json();
-  localStorage.setItem('usersData', JSON.stringify(data));
-  displayUsers();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem('usersData') !== null) {
-    console.log('ключ есть загрузки не будет')
-    displayUsers();
+document.addEventListener('DOMContentLoaded', async () => {
+  const currentUsers = getLocalData();
+  if (currentUsers.length > 0) {
+    displayUsers(currentUsers);
   } else {
-    loadUsers();
+    const loaded = await loadUsers();
+    displayUsers(loaded);
   }
 });
-
-
-
-
